7c91e8db11e65602678d7fc1f279111f86346ed5
SkyMazter <comunidadoscar@gmail.com>
2024-07-24 16:12:12 -0400

    Setup project code

---
+++ SYEP-24-StoryMap/src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import Button from 'react-bootstrap/Button';
+function App() {
+  return (
+    <>
+      <div>
+        <h1>This is a header</h1>
+        <p>This is a paragraph</p>
+        <button>this is a button</button>
+        <Button>Click me</Button>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ SYEP-24-StoryMap/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
