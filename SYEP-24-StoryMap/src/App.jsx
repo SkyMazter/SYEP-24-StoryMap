1e7e37c4897537cd828b3254be12a993990fce15
SkyMazter <comunidadoscar@gmail.com>
2024-07-24 16:46:41 -0400

    updated code

--- SYEP-24-StoryMap/src/App.jsx
+++ SYEP-24-StoryMap/src/App.jsx
@@ -1,13 +1,29 @@
 import "./App.css";
-import Button from 'react-bootstrap/Button';
+import Button from "react-bootstrap/Button";
 function App() {
   return (
     <>
       <div>
-        <h1>This is a header</h1>
-        <p>This is a paragraph</p>
-        <button>this is a button</button>
-        <Button>Click me</Button>
+        <h1>Title Of Website</h1>
+        <p> This is a paragraph </p>
+        <input></input>
+        <button>This is a button</button>
+        <h2>groceries</h2>
+        
+        <div className="bg-secondary">
+          <h3>other</h3>
+          <h4>other</h4>
+          <h5>other</h5>
+          <h6>other</h6>
+        </div>
+
+        <ol>
+          <li>eggs</li>
+          <li>Milk</li>
+          <li>Cereal</li>
+        </ol>
+
+        <li>Hello</li>
       </div>
     </>
   );
