675827fe1831aa8328951391a5cf8d6dcd7bdf4c
SkyMazter <comunidadoscar@gmail.com>
2024-07-25 15:04:11 -0400

    clean slate

--- SYEP-24-StoryMap/src/App.jsx
+++ SYEP-24-StoryMap/src/App.jsx
@@ -1,32 +1,11 @@
 import "./App.css";
-import Button from "react-bootstrap/Button";
+
+
 function App() {
   return (
-    <>
-      <div>
-        <h1>this is a website
-        </h1>
-        <p> Random nonsense </p>
-        <input></input>
-        <button>This is a button</button>
-        <h2>groceries</h2>
-        
-        <div className="bg-secondary">
-          <h3>other</h3>
-          <h4>other</h4>
-          <h5>other</h5>
-          <h6>other</h6>
-        </div>
-
-        <ol>
-          <li>eggs</li>
-          <li>Milk</li>
-          <li>Cereal</li>
-        </ol>
-
-        <li>Hello</li>
-      </div>
-    </>
+    <div>
+      
+    </div>
   );
 }
 
