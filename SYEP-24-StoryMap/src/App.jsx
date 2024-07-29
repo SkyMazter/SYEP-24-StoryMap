90a2bff277283c31a36694b7ce02d26f77da3592
jarlenem01 <jarlenemelendez01@gmail.com>
2024-07-29 14:43:13 -0400

    Finished home page

--- SYEP-24-StoryMap/src/App.jsx
+++ SYEP-24-StoryMap/src/App.jsx
@@ -1,10 +1,17 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import Accordion from 'react-bootstrap/Accordion';
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import Carousel from 'react-bootstrap/Carousel';
+import ExampleCarouselImage from 'components/ExampleCarouselImage';
+import Dropdown from 'react-bootstrap/Dropdown';
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
 function App() {
   return (
     <>
       <div>
-        <h1>This i</h1>
+      <h1>This i</h1>
         <p>This is a paragraph</p>
         <button>this is a button</button>
         <Button>Click me</Button>
