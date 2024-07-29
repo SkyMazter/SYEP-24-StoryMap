2d167e7e05aecebf717dc631a4cfc139a4c8c329
jarlenem01 <jarlenemelendez01@gmail.com>
2024-07-29 14:32:08 -0400

    Finished home page

--- SYEP-24-StoryMap/src/App.jsx
+++ SYEP-24-StoryMap/src/App.jsx
@@ -1,32 +1,36 @@
 import "./App.css";
 import Button from "react-bootstrap/Button";
+import Breadcrumb from 'react-bootstrap/Breadcrumb';
+import BreadcrumbItem from "react-bootstrap/esm/BreadcrumbItem";
+import Image from 'react-bootstrap/Image';
+import Card from 'react-bootstrap/Card';
+import Placeholder from 'react-bootstrap/Placeholder';
+import CardBody from "react-bootstrap/esm/CardBody";
+import Accordion from 'react-bootstrap/Accordion';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Spinner from 'react-bootstrap/Spinner';
+
 function App() {
   return (
     <>
       <div>
-        <h1>Extra Branch</h1>
-        <p> This is a paragraph </p>
-        <input></input>
-        <button>This is a button</button>
-        <h2>groceries</h2>
-        
-        <div className="bg-secondary">
-          <h3>other</h3>
-          <h4>other</h4>
-          <h5>other</h5>
-          <h6>other</h6>
-        </div>
-
-        <ol>
-          <li>eggs</li>
-          <li>Milk</li>
-          <li>Cereal</li>
-        </ol>
-
-        <li>Hello</li>
+        <h1>This i</h1>
+        <p>This is a paragraph</p>
+        <button>this is a button</button>
+        <Button>Click me</Button>
       </div>
-    </>
-  );
+        <Button variant="Link">Storymap</Button>
+        <ListGroup as="ul">
+      <ListGroup.Item as="li" active>Zines
+      </ListGroup.Item>
+      <ListGroup.Item as="li">PSA Videos</ListGroup.Item>
+      <ListGroup.Item as="li" disabled> Climate Change
+      </ListGroup.Item>
+      <ListGroup.Item as="li">Air Quality</ListGroup.Item>
+    </ListGroup>
+    <Spinner animation="grow" variant="primary" />
+    </
+  )
 }
 
 export default App;
