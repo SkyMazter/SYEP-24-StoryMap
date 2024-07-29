d3451e58b1884c171c8958479a22d80a6a4e9592
SkyMazter <comunidadoscar@gmail.com>
2024-07-29 15:11:09 -0400

    Finished home page

--- SYEP-24-StoryMap/src/App.jsx
+++ SYEP-24-StoryMap/src/App.jsx
@@ -1,64 +1,175 @@
 import "./App.css";
-import Accordion from "react-bootstrap/Accordion" 
-import Table from "react-bootstrap/Table"
-import Image from 'react-bootstrap/Image'
-import AccordionHeader from "react-bootstrap/esm/AccordionHeader";
-function App(){
-  return(
-    <div>
-      <h1>Air Quality</h1>
-    <p></p>
-    <h4>Details on Air Quality / Pollution </h4>
-<Accordion defaultActiveKey="0">
-  <Accordion.Item eventKey="0">
-   <AccordionHeader>What is Air Quality? Who does it affect?</AccordionHeader>
-    <Accordion.Body>
-    Air quality is the degree to which the air is suitable and clean for humans, animals and the environment. Air that is free from toxic and harmful substances. Air quality affects everyone no matter how far or close you are near a pollution hotspot. 
-     </Accordion.Body>
-      </Accordion.Item>
-       <Accordion.Item eventKey="1">
-      <Accordion.Header>Why is Air Quality Important? </Accordion.Header>
-     <Accordion.Body>
-    Humans and the environment need air and breathe a lot of it, and breathing in such harsh conditions of air will only lead to health problems and a poor environment. Harmful air quality -causes certain symptoms such as irritation to the eyes, nose, throat and even shortness of breath. The more you are exposed to such harsh environments the more health problems you can or will possibly face in your future. Poor air quality can slowly deteriorate your physical and mental health if exposed constantly to it. 
-    </Accordion.Body>
-  </Accordion.Item>
-</Accordion>
-<p></p>
-<p></p>
-<h5>
-         Air Quality Scale 
-  </h5>
-  <Table striped>
-      <thead>
-        <tr>
-          <th>Good</th>
-          <th>Moderate</th>
-          <th>Unhealthy</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>0-49</td>
-          <td>50-100</td>
-          <td>101-150</td>
-        </tr>
-        <tr>
-        </tr>
-      </tbody>
-    </Table>
- <p></p>
 
-    <Accordion defaultActiveKey={['0']} alwaysOpen>
-      <Accordion.Item eventKey="0">
-      <h5>Air Quality in Williamsburg</h5>
-      <Accordion.Body>
-Williamsburg, NY Air Quality forecast has been extremely poor and has a consistent air quality of around the 60s which is moderate for breathing and is significantly lower than good air which is anything above 50. Currently the community of Williamsburg has been taking in decent levels of nitrogen dioxide which then harms them and increases issues such as respiratory problems and more. People of williamsburg are most likely taking in as much nitrogen dioxide than oxygen. 
-        </Accordion.Body>
-      </Accordion.Item>
-    </Accordion>
-    <h5>Air Quality and the BQE</h5>
-  </div>
-  )
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Figure from "react-bootstrap/Figure";
+import Image from "react-bootstrap/Image";
+
+import { Link } from "react-router-dom";
+import img from "./assets/placeholder.png";
+
+function App() {
+  return (
+    <div>
+      <Container className="p-3">
+        <Row>
+          <Col className="justify-content-center d-flex">
+            <h3>Southside StoryMap</h3>
+          </Col>
+        </Row>
+        <Row>
+          <Col className="justify-content-center d-flex" xs={3}>
+            <Link>Home</Link>
+          </Col>
+          <Col className="justify-content-center d-flex" xs={3}>
+            <Link>StoryMap</Link>
+          </Col>
+          <Col className="justify-content-center d-flex" xs={3}>
+            <Link>SoundScapes</Link>
+          </Col>
+          <Col className="justify-content-center d-flex" xs={3}>
+            <Link>Muralistas</Link>
+          </Col>
+        </Row>
+      </Container>
+      <div
+        className="bg-primary w-100 justify-content-center d-flex align-items-center"
+        style={{
+          position: "relative",
+        }}
+      >
+        <h3
+          style={{
+            position: "absolute",
+            zIndex: 3,
+          }}
+        >
+          {" "}
+          Subheader
+        </h3>
+        <Image
+          src={img}
+          style={{
+            height: "200px",
+            width: "100%",
+            position: "relative",
+            zIndex: 1,
+          }}
+        ></Image>
+      </div>
+      <Container className="p-3">
+        <Row>
+          <Col>
+            <h4>Title</h4>
+          </Col>
+        </Row>
+        <Row>
+          <Col xs={12} md={4}>
+            <Figure>
+              <Figure.Image
+                width={500}
+                height={100}
+                alt="171x180"
+                src={img}
+                fluid
+              />
+              <Figure.Caption>syep cohort 2024</Figure.Caption>
+            </Figure>
+          </Col>
+          <Col xs={12} md={8}>
+            {" "}
+            <p>
+              {" "}
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Dignissim
+              pulvinar venenatis habitasse per tellus sociosqu eros rhoncus
+              adipiscing. Elit dignissim imperdiet ac massa facilisi quam
+              sollicitudin conubia. Cubilia efficitur litora magna amet; magnis
+              cras integer. Est eget commodo feugiat quisque facilisi dis lorem
+              proin. Tellus sagittis lobortis risus fusce ridiculus penatibus
+              volutpat integer. Lorem ipsum odor amet, consectetuer adipiscing
+              elit. Dignissim pulvinar venenatis habitasse per tellus sociosqu
+              eros rhoncus adipiscing. Elit dignissim imperdiet ac massa
+              facilisi quam sollicitudin conubia. Cubilia efficitur litora magna
+              amet; magnis cras integer. Est eget commodo feugiat quisque
+              facilisi dis lorem proin. Tellus sagittis lobortis risus fusce
+              ridiculus penatibus volutpat integer.
+            </p>{" "}
+            <p>
+              {" "}
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Dignissim
+              pulvinar venenatis habitasse per tellus sociosqu eros rhoncus
+              adipiscing. Elit dignissim imperdiet ac massa facilisi quam
+              sollicitudin conubia. Cubilia efficitur litora magna amet; magnis
+              cras integer. Est eget commodo feugiat quisque facilisi dis lorem
+              proin. Tellus sagittis lobortis risus fusce ridiculus penatibus
+              volutpat integer. Lorem ipsum odor amet, consectetuer adipiscing
+              elit. Dignissim pulvinar venenatis habitasse per tellus sociosqu
+              eros rhoncus adipiscing. Elit dignissim imperdiet ac massa
+              facilisi quam sollicitudin conubia. Cubilia efficitur litora magna
+              amet; magnis cras integer. Est eget commodo feugiat quisque
+              facilisi dis lorem proin. Tellus sagittis lobortis risus fusce
+              ridiculus penatibus volutpat integer.
+            </p>{" "}
+          </Col>
+        </Row>
+      </Container>
+
+      <Container className="p-3">
+        <Row>
+          <Col xs={12} sm={4} md={{ order: "last" }}>
+            <Figure>
+              <Figure.Image
+                width={500}
+                height={100}
+                alt="171x180"
+                src={img}
+                fluid
+              />
+              <Figure.Caption>syep cohort 2024</Figure.Caption>
+            </Figure>
+          </Col>
+          <Col xs={12} md={8}>
+            {" "}
+            <p>
+              {" "}
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Dignissim
+              pulvinar venenatis habitasse per tellus sociosqu eros rhoncus
+              adipiscing. Elit dignissim imperdiet ac massa facilisi quam
+              sollicitudin conubia. Cubilia efficitur litora magna amet; magnis
+              cras integer. Est eget commodo feugiat quisque facilisi dis lorem
+              proin. Tellus sagittis lobortis risus fusce ridiculus penatibus
+              volutpat integer. Lorem ipsum odor amet, consectetuer adipiscing
+              elit. Dignissim pulvinar venenatis habitasse per tellus sociosqu
+              eros rhoncus adipiscing. Elit dignissim imperdiet ac massa
+              facilisi quam sollicitudin conubia. Cubilia efficitur litora magna
+              amet; magnis cras integer. Est eget commodo feugiat quisque
+              facilisi dis lorem proin. Tellus sagittis lobortis risus fusce
+              ridiculus penatibus volutpat integer.
+            </p>{" "}
+            <p>
+              {" "}
+              Lorem ipsum odor amet, consectetuer adipiscing elit. Dignissim
+              pulvinar venenatis habitasse per tellus sociosqu eros rhoncus
+              adipiscing. Elit dignissim imperdiet ac massa facilisi quam
+              sollicitudin conubia. Cubilia efficitur litora magna amet; magnis
+              cras integer. Est eget commodo feugiat quisque facilisi dis lorem
+              proin. Tellus sagittis lobortis risus fusce ridiculus penatibus
+              volutpat integer. Lorem ipsum odor amet, consectetuer adipiscing
+              elit. Dignissim pulvinar venenatis habitasse per tellus sociosqu
+              eros rhoncus adipiscing. Elit dignissim imperdiet ac massa
+              facilisi quam sollicitudin conubia. Cubilia efficitur litora magna
+              amet; magnis cras integer. Est eget commodo feugiat quisque
+              facilisi dis lorem proin. Tellus sagittis lobortis risus fusce
+              ridiculus penatibus volutpat integer.
+            </p>{" "}
+          </Col>
+        </Row>
+      </Container>
+
+
+    </div>
+  );
 }
 
 export default App;
