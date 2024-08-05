32b755ae427a017a0bcf59d231036f7d890243e8
jarlenem01 <jarlenemelendez01@gmail.com>
2024-08-05 16:54:34 -0400

    links

--- SYEP-24-StoryMap/src/pages/Storymap.jsx
+++ SYEP-24-StoryMap/src/pages/Storymap.jsx
@@ -1,9 +1,9 @@
 import "../styles/StoryMap.css";
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Figure from 'react-bootstrap/Figure';
-import Box from '../assets/placeholder.png'
-import Carousel from 'react-bootstrap/Carousel';
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Figure from "react-bootstrap/Figure";
+import Box from "../assets/placeholder.png";
+import Carousel from "react-bootstrap/Carousel";
 import CarouselCaption from "react-bootstrap/esm/CarouselCaption";
 import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
@@ -28,12 +28,11 @@ const Storymap = () => {
     <div>
       <NavBar />
       <Link to={"/"}>Go Home</Link>
-      <h1 className="quando-regular">Reviving The BQE</h1 >
+      <h1 className="quando-regular">Reviving The BQE</h1>
 
       <Container className="p-3">
         <Row>
-          <Col>
-          </Col>
+          <Col></Col>
         </Row>
         <Row>
           <Col xs={12} md={4}>
@@ -45,35 +44,51 @@ const Storymap = () => {
                 src={BQEMap}
                 fluid
               />
-              <Figure.Caption className="judson-regular">Background of The BQE
+              <Figure.Caption className="judson-regular">
+                Route of The BQE
               </Figure.Caption>
-              <Button variant="link">More</Button>{' '}
+              <Button variant="link">
+                {" "}
+                <Link to={"https://bqevision.com/about/background"}>More</Link>
+              </Button>{" "}
             </Figure>
           </Col>
           <Col xs={12} md={8}>
             {" "}
             <p className="judson-regular">
               {" "}
-              The Brooklyn-Queens Expressway is an 11.7 mile long expressway, extending from the Brooklyn-Battery Tunnel to the 
-              Grand Central Parkway in Queens. Designed and planned by Robert Moses, the construction
-             of the BQE lasted 27 years, beginning in 1937 and ending in 1964. Since then, the BQE has shaped New York City tremendously.
-             With approximately 130,000 vehicles using the BQE daily,it’s evident that the passageway
-             has become a staple in thousands of people’s daily lives. Whilst making people’s daily
-              commutes more efficient, life for those beside the BQE hasn’t been made as easy.
-              Air quality levels and city temperature’s double, 
-              the loud & irritable noises from the highway boom throughout the streets, intersections
-              and roadways become harder to cross. Year after year the communities that line the BQE become more
-              dangerous, forcing people to suffer more and more. The BQE, overall, has become a huge hazard
-             throughout these communities. These neighborhoods live everyday with the costs and risks of this legacy infrastructure
-              through poor air quality, divided communities, traffic violence, visual blight, and noise pollution – 
-              costs which depress economic and social opportunities and disproportionately fall on environmental justice 
-              communities. The BQE represents the paradox of Moses’ legacy. 
-              The roads, parks, bridges, and housing he constructed that reshaped New York City and Long Island to 
-              serve the public only hurt the public. These constructions resulted in the displacement of thousands of families, destruction of tight-knit 
-              neighborhoods, and overall were built at the expense of innumerable people and communities of color. 
-              The BQE is no exception. Today, as the aging highway crumbles, New York City needs to decide if there’s 
-              a better way forward. At El Puente we make it our everyday goal to do what we can to support
-              the efforts being made to bring environmental justice to these communities. One of these efforts being the BQGreen project.
+              The Brooklyn-Queens Expressway is an 11.7 mile long expressway,
+              extending from the Brooklyn-Battery Tunnel to the Grand Central
+              Parkway in Queens. Designed and planned by Robert Moses, the
+              construction of the BQE lasted 27 years, beginning in 1937 and
+              ending in 1964. Since then, the BQE has shaped New York City
+              tremendously. With approximately 130,000 vehicles using the BQE
+              daily,it’s evident that the passageway has become a staple in
+              thousands of people’s daily lives. Whilst making people’s daily
+              commutes more efficient, life for those beside the BQE hasn’t been
+              made as easy. Air quality levels and city temperature’s double,
+              the loud & irritable noises from the highway boom throughout the
+              streets, intersections and roadways become harder to cross. Year
+              after year the communities that line the BQE become more
+              dangerous, forcing people to suffer more and more. The BQE,
+              overall, has become a huge hazard throughout these communities.
+              These neighborhoods live everyday with the costs and risks of this
+              legacy infrastructure through poor air quality, divided
+              communities, traffic violence, visual blight, and noise pollution
+              – costs which depress economic and social opportunities and
+              disproportionately fall on environmental justice communities. The
+              BQE represents the paradox of Moses’ legacy. The roads, parks,
+              bridges, and housing he constructed that reshaped New York City
+              and Long Island to serve the public only hurt the public. These
+              constructions resulted in the displacement of thousands of
+              families, destruction of tight-knit neighborhoods, and overall
+              were built at the expense of innumerable people and communities of
+              color. The BQE is no exception. Today, as the aging highway
+              crumbles, New York City needs to decide if there’s a better way
+              forward. At El Puente we make it our everyday goal to do what we
+              can to support the efforts being made to bring environmental
+              justice to these communities. One of these efforts being the
+              BQGreen project.
             </p>
           </Col>
         </Row>
@@ -81,80 +96,79 @@ const Storymap = () => {
 
       <Carousel>
         <Carousel.Item>
-          <div className="w-100 d-flex justify-content-center align-items-center">
-          <Image fluid src={BQEWill}></Image>
+          <div className=" d-flex justify-content-center align-items-center carousellImage">
+            <Image fluid src={BQEWill}></Image>
           </div>
+          <Carousel.Caption>
+            <h4 className="judson-regular">The BQE</h4>
+          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Caption>
-          <h4 className="judson-regular">The BQE</h4>
-        </Carousel.Caption>
+
         <Carousel.Item>
-        <div className="w-100 d-flex justify-content-center align-items-center">
-          <Image fluid src={Promenade}></Image>
+          <div className=" d-flex justify-content-center align-items-center carousellImage">
+            <Image fluid src={Promenade}></Image>
           </div>
+          <Carousel.Caption>
+            <h4 className="judson-regular">The BQE</h4>
+          </Carousel.Caption>
         </Carousel.Item>
-        <Carousel.Caption>
-          <h4 className="judson-regular">The BQE</h4>
-        </Carousel.Caption>
+
         <Carousel.Item>
-        <div className="w-80 d-flex justify-content-center align-items-center">
-          <Image fluid src={Highway2}></Image>
+          <div className=" d-flex justify-content-center align-items-center carousellImage">
+            <Image fluid src={Highway2}></Image>
           </div>
         </Carousel.Item>
-        <Carousel.Caption>
-          <h4 className="judson-regular">The BQE</h4>
-        </Carousel.Caption>
       </Carousel>
 
       <div>
-      <Container className="p-3">
-        <Row>
-          <Col>
-          </Col>
-        </Row>
-        <Row>
-          <Col xs={12} md={8}>
-            {" "}
-            <p className="judson-regular">
+        <Container className="p-3">
+          <Row>
+            <Col></Col>
+          </Row>
+          <Row>
+            <Col xs={12} md={8}>
               {" "}
-              The BQGreen project is one of the many projects whose goal is 
-              to restore health, peace, and safety back into our communities. The 
-              project's plan is to cap a section of the BQE in Williamsburg and 
-              construct several parks. This plan will help to increase the 
-              amount of green & open community spaces in the community, 
-              which will help the organization work towards their goals 
-              of delivering environmental justice to the community.
-              The project will help promote improved health for current
-             and future generations thanks to the trees and plants that
-              will help absorb freeway noise and dirty air, and ease the
-             urban heat island effect. It's a high profile idea that would
-              make a significant difference citywide. Even more, the project 
-              is a great example of what can 
-             be achieved when local communities, city, state and federal 
-             governments and private companies work together toward a more 
-             equitable and sustainable future. Deputy Borough President 
-             Diana Reyna is the project’s leading public sponsor and its 
-             original champion. Other leading advocates of BQGreen include 
-             local Council Member Antonio Reynoso, Council Member Mark 
-             Levine, chair of the City Council’s Committee on Parks and 
-             Recreation and Susannah Drake, the project’s architect.
-            </p>
-          </Col>
-          <Col xs={12} md={4}>
-            <Figure>
-              <Figure.Image
-                width={500}
-                height={100}
-                alt="171x180"
-                src={BQGreenpic}
-                fluid
-              />
-              <Figure.Caption className="judson-regular">The BQGreen Project</Figure.Caption>
-              <Button variant="link">More</Button>{' '}
-            </Figure>
-          </Col>
-        </Row>
-      </Container>
+              <p className="judson-regular">
+                {" "}
+                The BQGreen project is one of the many projects whose goal is to
+                restore health, peace, and safety back into our communities. The
+                project's plan is to cap a section of the BQE in Williamsburg
+                and construct several parks. This plan will help to increase the
+                amount of green & open community spaces in the community, which
+                will help the organization work towards their goals of
+                delivering environmental justice to the community. The project
+                will help promote improved health for current and future
+                generations thanks to the trees and plants that will help absorb
+                freeway noise and dirty air, and ease the urban heat island
+                effect. It's a high profile idea that would make a significant
+                difference citywide. Even more, the project is a great example
+                of what can be achieved when local communities, city, state and
+                federal governments and private companies work together toward a
+                more equitable and sustainable future. Deputy Borough President
+                Diana Reyna is the project’s leading public sponsor and its
+                original champion. Other leading advocates of BQGreen include
+                local Council Member Antonio Reynoso, Council Member Mark
+                Levine, chair of the City Council’s Committee on Parks and
+                Recreation and Susannah Drake, the project’s architect.
+              </p>
+            </Col>
+            <Col xs={12} md={4}>
+              <Figure>
+                <Figure.Image
+                  width={500}
+                  height={100}
+                  alt="171x180"
+                  src={BQGreenpic}
+                  fluid
+                />
+                <Figure.Caption className="judson-regular">
+                  The BQGreen Project
+                </Figure.Caption>
+                <Button variant="link">More</Button>{" "}
+              </Figure>
+            </Col>
+          </Row>
+        </Container>
       </div>
 
       <Container className="climate-change" fluid>
@@ -377,4 +391,4 @@ const Storymap = () => {
   );
 };
 
-export default Storymap
+export default Storymap;
