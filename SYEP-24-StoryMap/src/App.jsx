358cba71e2bbe891c57810d459122e0c4203b1f9
ctny6 <ctny6@ctny6s-MacBook-Air.local>
2024-08-08 17:44:51 -0400

    edited the baackground color

--- SYEP-24-StoryMap/src/App.jsx
+++ SYEP-24-StoryMap/src/App.jsx
@@ -20,27 +20,30 @@ import NavBar from "./components/NavBar.jsx";
 function App() {
   return (
     <div>
-      <Container fluid >
+      <Container fluid style={{ backgroundColor: "#265F7E" }}>
         <Row>
-          <Col md={3}>
-            <Image fluid src= {bridge}></Image>
+          <Col md={3} className="p-0">
+            <Image fluid src={bridge} className="w-100 h-100"></Image>
           </Col>
-          <Col md={9}>
-            <Container fluid>
+          <Col md={9} className="p-0">
+            <Container fluid style={{ backgroundColor: "#265F7E" }}>
               {" "}
               <Row>
                 {" "}
-                <Col xs={12}>
+                <Col xs={12} className="p-0">
                   <NavBar></NavBar>
                 </Col>
               </Row>
               <Row>
                 <Col>
                   {" "}
-                  <h1 className="quando-regular text-center">
+                  <h1
+                    className="quando-regular text-center"
+                    style={{ color: "white" }}
+                  >
                     SouthSide Story Map
                   </h1>
-                  <p className="judson-regular">
+                  <p className="judson-regular" style={{ color: "white" }}>
                     The Summer Youth Employment Program, is a national program
                     that connects youth, ages 14-21, to paid internships of
                     their choice. This program allows youth to gain professional
@@ -52,7 +55,7 @@ function App() {
               <Row>
                 <Col>
                   {" "}
-                  <p className="judson-regular">
+                  <p className="judson-regular" style={{ color: "white" }}>
                     {" "}
                     Throughout the 6 weeks of working at El Puente, youth
                     employees have worked on canvassing to local residents,
@@ -70,24 +73,33 @@ function App() {
               <Row>
                 <Col className="d-flex align-items-center justify-content-center">
                   {" "}
-                  <Image fluid src={art}></Image>
+                  <Image fluid src={art} className="w-100 h-100"></Image>
                 </Col>
                 <Col className="d-flex align-items-center justify-content-center">
                   <Image fluid src={microphone}></Image>
                 </Col>
                 <Col className="d-flex align-items-center justify-content-center">
-                  <Image fluid src={water}></Image>
+                  <Image fluid src={water} className="w-100 h-100"></Image>
                 </Col>
               </Row>
-              <Row className="quando-regular text-center">
+              <Row
+                className="quando-regular text-center"
+                style={{ color: "white" }}
+              >
                 {" "}
                 <h1>About us</h1>
               </Row>
               <Row>
-                <Col>
-                  <Image fluid src={circleart} className="w-50 justify-content-center"></Image>
+                <Col xs={12}
+                  md={4}>
+                  <Image fluid src={circleart} className="w-75 "></Image>
                 </Col>
-                <Col className="quando-regular text-center justify-content-center">
+                <Col
+                  className="judson-regular text-center justify-content-center p-2 "
+                  style={{ color: "white" }}
+                  xs={12}
+                  md={8}
+                >
                   {" "}
                   <p>
                     Gravida phasellus vulputate lobortis efficitur tempus lectus
@@ -108,7 +120,7 @@ function App() {
           </Col>
         </Row>
         <Row>
-          <Col>
+          <Col className="p-0">
             <Footer></Footer>
           </Col>
         </Row>
