d989b3d10acbffdfbff443cfa2fa5afb93c8fb6c
Mckayla <mckaylacrrn@gmail.com>
2024-08-15 13:46:38 -0400

    saving changes

--- SYEP-24-StoryMap/src/pages/Muralistas.jsx
+++ SYEP-24-StoryMap/src/pages/Muralistas.jsx
@@ -7,27 +7,40 @@ import Col from "react-bootstrap/Col";
 import Carousel from "react-bootstrap/Carousel";
 import Placeholder2 from "../assets/blank.png";
 import SYEPArt from "../assets/syepart.jpg";
+import art1 from "../assets/art 1.jpg";
+import art2 from "../assets/art 2.jpg";
+import yoseff from "../assets/yosef.jpg";
+import sandb from "../assets/shay and brit.jpg";
+import art3 from "../assets/art 3.jpg";
+import library from "../assets/img_3050.jpg";
+import painting from "../assets/denise.jpg";
+import ypaint from "../assets/yosefff.jpg";
+import yandd from "../assets/yandd.jpg";
+import cyano from "../assets/cyano.jpg";
+import cyano2 from "../assets/cyano2.jpg";
+import posters2 from "../assets/posters2.jpg";
+import pinata from "../assets/pinatas.jpg";
 const Muralistas = () => {
   return (
     <div>
       <NavBar></NavBar>
-      <Carousel fluid style={{ backgroundColor: "#83AEAE", color: "white" }}>
+      <Carousel style={{ backgroundColor: "#0E5489", color: "white" }}>
         <Carousel.Item>
           {" "}
-          <Image fluid className="muralistaimage" src={Placeholder2}></Image>
-          <Carousel.Caption>The first image</Carousel.Caption>
+          <Image fluid className="yandd" src={posters2}></Image>
+          <Carousel.Caption>Using the locations from El Puente Green Light District’s Environmental Justice Walk, the art group did “The Walk” activity by Lynda Barry. </Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
           {" "}
-          <Image fluid className="muralistaimage" src={Placeholder2}></Image>
-          <Carousel.Caption>The second image</Carousel.Caption>
+          <Image fluid className="yandd" src={yandd}></Image>
+          <Carousel.Caption>Finishing process of the Cyanotype prints drying in the sun</Carousel.Caption>
         </Carousel.Item>
 
         <Carousel.Item>
           {" "}
-          <Image fluid className="muralistaimage" src={Placeholder2}></Image>
-          <Carousel.Caption>The third image</Carousel.Caption>
+          <Image fluid className="yandd" src={cyano}></Image>
+          <Carousel.Caption>Cyanotypes that our SYEP art group created</Carousel.Caption>
         </Carousel.Item>
       </Carousel>
 
@@ -39,10 +52,12 @@ const Muralistas = () => {
         <div class="container text-center">
           <div class="row">
             <div class="col">
-              <h5 className="quando-regular">Muralistas Group 2024</h5>
+              <h5 className="quando-regular">Placemaking Through Art in Los Sures</h5>
             </div>
             <div class="col">
-             <h5 className="judson-regular">The artists.. Yosef, Denise, Shaylie, and Pierre from El Puente</h5>
+              <h5 className="judson-regular">
+                The artists.. Yosef, Denise, Shaylie, and Pierre from El Puente
+              </h5>
             </div>
           </div>
         </div>
@@ -52,29 +67,44 @@ const Muralistas = () => {
         className="p-2"
         fluid
         style={{ backgroundColor: "#0E5489", color: "white" }}
-      ><p className="my-3"></p>
-      
-        <div class fluid="container text-center">
+      >
+        <div class="container text-center">
           <div class="row">
+            <div class fluid="col">
+              <h4 className="quando-regular">Summer Art Gallery</h4>
+            </div>
+            <p className="my-1"></p>
+
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={sandb}></Image>
             </div>
 
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
-            </div>
-            <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={library}></Image>
             </div>
 
             <div class="col">
-              <p className="judson-regular">desc...</p>
+              <Image fluid src={art2}></Image>
+            </div>
+
+            <div class="col">
+              <Image fluid src={painting}></Image>
             </div>
           </div>
         </div>
         <p className="my-3"></p>
         <center>
-          <h5 className="quando-regular">Summer Art Gallery</h5>
+          <h4 className="quando-regular">Community Library</h4>
+          <p className="my-2"></p>
+          <p className="judson-regular">
+            The EL PUENTE SYEP summer artists 2024 core created a free community
+            library for the community of Los Sures. It is a symbol for
+            collaboration with the community and a tool to share resources such
+            as books and information. The library was painted, designed and
+            inspired by the youth artist group of SYEP youth members,
+            incorporating themes promoting reading, environmental awareness and
+            unity.
+          </p>
         </center>
       </Container>
 
@@ -82,21 +112,21 @@ const Muralistas = () => {
         <div class fluid="container text-center">
           <div class="row">
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={pinata}></Image>
             </div>
 
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={yoseff}></Image>
             </div>
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={cyano2}></Image>
             </div>
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={art3}></Image>
             </div>
 
             <div class="col">
-              <Image fluid src={Placeholder}></Image>
+              <Image fluid src={art1}></Image>
             </div>
           </div>
         </div>
@@ -104,7 +134,7 @@ const Muralistas = () => {
       </Container>
 
       <Container
-        className="p-3"
+        className="p-4"
         fluid
         style={{ backgroundColor: "#0E5489", color: "white" }}
       >
@@ -113,16 +143,32 @@ const Muralistas = () => {
             <div class="col">
               <Image src={SYEPArt}></Image>
               <p className="my-3"></p>
-              <h6 className="quando-regular">Art by Yosef Naranjo</h6> 
+              <h6 className="quando-regular">Art by Yosef Naranjo</h6>
             </div>
 
             <div class="col">
-              <p className="judson-regular">Our Southside artists are a group of youth organizers at El Puente
-              that came together to create fillerrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrr</p>
+              {" "}
+              <h4 className="quando-regular">Cyanotype Zine</h4>
+              <p className="judson-regular">
+                {" "}
+                This summer’s Cyanotype zine was a collectively directed
+                project. We looked at examples of zines and were inspired by
+                members within our summer group that made their own zine.
+                Ultimately, the group decided on an environmentally friendly art
+                making process that wouldn’t produce more waste in its
+                fabrication. Cyanotypes came forward because it relies on the
+                environment to produce it - it relies on sun exposure for their
+                images to transfer on to a fabric or paper. The materials we
+                used were either in house or reused from Materials for the arts,
+                except for our Cyanotype dye. The final images are related to
+                some aspects of our environmental justice campaign sites,
+                prompted by environmental awareness and advocacy within these
+                sites.
+              </p>
             </div>
-            </div>
-            </div>
-            </Container>
+          </div>
+        </div>
+      </Container>
     </div>
   );
 };
